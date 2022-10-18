4cf79ce742822dad3422c3e564fd098c3bd8a973
proninvladislav@gmail.com <proninvladislav@gmail.com>
2022-10-18 13:56:33 +0300

    one more chance

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -6,53 +6,53 @@ app_name = 'posts'
 
 urlpatterns = [
     path(
-     '',
-     views.index,
-     name='index'
+       '',
+       views.index,
+       name='index'
     ),
     path(
-     'group/<slug:slug>/',
-     views.group_posts,
-     name='group_posts'
+        'group/<slug:slug>/',
+        views.group_posts,
+       name='group_posts'
     ),
     path(
-     'profile/<str:username>/',
-     views.profile,
-     name='profile'
+        'profile/<str:username>/',
+        views.profile,
+        name='profile'
     ),
     path(
-     'posts/<int:post_id>/',
-     views.post_detail,
-      name='post_detail'
+        'posts/<int:post_id>/',
+        views.post_detail,
+         name='post_detail'
     ),
     path(
-     'create/',
-     views.post_create,
-     name='post_create'
+        'create/',
+        views.post_create,
+        name='post_create'
     ),
     path(
-     'posts/<int:post_id>/edit/',
-     views.post_edit,
-     name='post_edit'
+        'posts/<int:post_id>/edit/',
+        views.post_edit,
+        name='post_edit'
     ),
     path(
-     'posts/<int:post_id>/comment/',
-     views.add_comment,
-     name='add_comment'
+        'posts/<int:post_id>/comment/',
+        views.add_comment,
+        name='add_comment'
     ),
     path(
-     'follow/',
-     views.follow_index,
-     name='follow_index'
+        'follow/',
+        views.follow_index,
+        name='follow_index'
     ),
     path(
-     'profile/<str:username>/follow/',
-     views.profile_follow,
-     name='profile_follow'
+        'profile/<str:username>/follow/',
+        views.profile_follow,
+        name='profile_follow'
     ),
     path(
-     'profile/<str:username>/unfollow/',
-     views.profile_unfollow,
-     name='profile_unfollow'
+        'profile/<str:username>/unfollow/',
+        views.profile_unfollow,
+        name='profile_unfollow'
     ),
 ]
