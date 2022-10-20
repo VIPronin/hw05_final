2e307d04cf9b4a0415a9e4e97606c792d1c9034c
proninvladislav@gmail.com <proninvladislav@gmail.com>
2022-10-21 00:22:28 +0300

    one more commit

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -31,22 +31,19 @@ def group_posts(request, slug):
     return render(request, 'posts/group_list.html', context)
 
 
-@login_required()
 def profile(request, username):
-    if request.user.is_authenticated:
-        title = 'Профиль'
-        author = get_object_or_404(User, username=username)
-        page_obj = get_paginator(request, author.posts.all())
-        following = (Follow.objects.filter(user=request.user,
-                                           author=author).exists())
-        context = dict(
-            author=author,
-            following=following,
-            page_obj=page_obj,
-            title=title
-        )
-        return render(request, 'posts/profile.html', context)
-    return render(request)
+    title = 'Профиль'
+    author = get_object_or_404(User, username=username)
+    page_obj = get_paginator(request, author.posts.all())
+    following = (Follow.objects.filter(user=request.user,
+                                       author=author).exists())
+    context = dict(
+        author=author,
+        following=following,
+        page_obj=page_obj,
+        title=title
+    )
+    return render(request, 'posts/profile.html', context)
 
 
 def post_detail(request, post_id):
