c216e49c2527d58602c432d71535b9c2da6c1233
proninvladislav@gmail.com <proninvladislav@gmail.com>
2022-10-21 14:47:32 +0300

    nmore

--- yatube/posts/utils.py
+++ yatube/posts/utils.py
@@ -3,8 +3,8 @@ from django.core.paginator import Paginator
 from .constants import POSTS_PER_PAGE
 
 
-def get_paginator(request, posts):
-    paginator = Paginator(posts, POSTS_PER_PAGE)  # Показывать по 10  .
+def get_paginator(request, QuerySet):
+    paginator = Paginator(QuerySet, POSTS_PER_PAGE)  # Показывать по 10  .
     page_number = request.GET.get('page')  # Из URL извл. № стр-знач пар-а
     page_obj = paginator.get_page(page_number)  # набор записей
     return page_obj
